ffbaa2e298d00b671e4f600055362e93aa79f3ac
ucews <denelfie@gmail.com>
2016-05-10 17:59:34 +0200

    added testIllegalArgumentForImageRotate()

--- iMage/jmjrst.main/src/test/java/jmjrst/main/Tester.java
+++ iMage/jmjrst.main/src/test/java/jmjrst/main/Tester.java
@@ -11,29 +11,61 @@ import javax.imageio.ImageIO;
 
 import org.jis.generator.Generator;
 import org.junit.Before;
+import org.junit.Test;
 
+/**
+ * This class is used to run tests
+ *
+ * @author ucews
+ * @version 1.0
+ */
 public class Tester {
-	
+
 	private Generator generator;
 	private BufferedImage buffImage;
-	
+
+	/**
+	 * This method initialises a Generator and reads a jpg-picture
+	 */
 	@Before
-	public void setUp(){
+	public void setUp() {
 		generator = new Generator(null, 0);
-		try{
+		try {
 			buffImage = ImageIO.read(new File("src/test/resources/picture.jpg"));
-		}catch(IOException exception){
+		} catch (IOException exception) {
 		}
 	}
-	
-	public void testRotateBufferedImage(){
+
+	/**
+	 * This method tests if rotateImage functions properly with an BufferedImage
+	 */
+	@Test
+	public void testRotateBufferedImage() {
 		BufferedImage img = new BufferedImage(10, 10, 10);
 		BufferedImage tmp = generator.rotateImage(img, 0.0);
 		assertEquals(tmp, img);
 	}
-	
-	public void testRotateNullImage(){
+
+	/**
+	 * This method tests if rotateImage functions properly with null
+	 */
+	@Test
+	public void testRotateNullImage() {
 		BufferedImage img = generator.rotateImage(null, 0.0);
 		assertEquals(img, null);
+	}
+	/**
+	 * This method tests if rotateImage throws an IllegalArgumentException
+	 */
+	@Test
+	public void testIllegalArgumentForImageRotate() {
+		boolean gotExecption = false;
+		try {
+			setUp();
+			generator.rotateImage(buffImage, 1.0);
+		} catch (IllegalArgumentException exception) {
+			gotExecption = true;
+		}
+		assertTrue(gotExecption);
 	}
 }
