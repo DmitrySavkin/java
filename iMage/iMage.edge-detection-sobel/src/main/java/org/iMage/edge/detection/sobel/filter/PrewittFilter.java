bd6b92e56de257e85807163909dd134280b34fb8
ucews <denelfie@gmail.com>
2016-05-23 06:53:25 +0200

    added applyFilter(BufferedImage image) to PrewittFilter.java

--- iMage/iMage.edge-detection-sobel/src/main/java/org/iMage/edge/detection/sobel/filter/PrewittFilter.java
+++ iMage/iMage.edge-detection-sobel/src/main/java/org/iMage/edge/detection/sobel/filter/PrewittFilter.java
@@ -14,10 +14,28 @@ public class PrewittFilter implements ImageFilter {
 		// TODO Auto-generated constructor stub
 	}
 
+	private int[][] prewit_x = { { -1, 0, 1 }, { -1, 0, 1 }, { -1, 0, 1 } };
+	private int[][] prewit_y = { { -1, -1, -1 }, { 0, 0, 0 }, { 1, 1, 1 } };
+
 	@Override
 	public BufferedImage applyFilter(BufferedImage image) {
-		// TODO Auto-generated method stub
-		return null;
+		BufferedImage result = image;
+		for (int i = 0; i < image.getWidth() - 1; i++) {
+			for (int j = 1; j < image.getHeight() - 1; j++) {
+				int pixel_x = (prewit_x[0][0] * image.getRGB(i - 1, j - 1)) + (prewit_x[0][1] * image.getRGB(i, j - 1))
+						+ (prewit_x[0][2] * image.getRGB(i + 1, j - 1)) + (prewit_x[1][0] * image.getRGB(i - 1, j))
+						+ (prewit_x[1][1] * image.getRGB(i, j)) + (prewit_x[1][2] * image.getRGB(i + 1, j))
+						+ (prewit_x[2][0] * image.getRGB(i - 1, j + 1)) + (prewit_x[2][1] * image.getRGB(i, j + 1))
+						+ (prewit_x[2][2] * image.getRGB(i + 1, j + 1));
+				int pixel_y = (prewit_y[0][0] * image.getRGB(i - 1, j - 1)) + (prewit_y[0][1] * image.getRGB(i, j - 1))
+						+ (prewit_y[0][2] * image.getRGB(i + 1, j - 1)) + (prewit_y[1][0] * image.getRGB(i - 1, j))
+						+ (prewit_y[1][1] * image.getRGB(i, j)) + (prewit_y[1][2] * image.getRGB(i + 1, j))
+						+ (prewit_y[2][0] * image.getRGB(i - 1, j + 1)) + (prewit_y[2][1] * image.getRGB(i, j + 1))
+						+ (prewit_y[2][2] * image.getRGB(i + 1, j + 1));
+				int value = (int) Math.floor(Math.sqrt((pixel_x * pixel_x) + (pixel_y * pixel_y)));
+				result.setRGB(i, j, value);
+			}
+		}
+		return result;
 	}
-
 }
--- iMage/iMage.edge-detection-sobel/src/main/java/org/iMage/edge/detection/sobel/filter/SobelFilter.java
+++ iMage/iMage.edge-detection-sobel/src/main/java/org/iMage/edge/detection/sobel/filter/SobelFilter.java
@@ -37,5 +37,4 @@ public class SobelFilter implements ImageFilter {
 		}
 		return result;
 	}
-
 }
