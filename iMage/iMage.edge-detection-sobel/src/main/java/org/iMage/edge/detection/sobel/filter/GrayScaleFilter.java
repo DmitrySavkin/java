df3363a689cac89f4e9b737b7b2fc10bfb288f1e
ucews <denelfie@gmail.com>
2016-05-23 03:58:30 +0200

    deleted module and added applyFilter(BufferedImage image) method to GrayScaleFilter.java

--- iMage/iMage.edge-detection-sobel/src/main/java/org/iMage/edge/detection/sobel/filter/GrayScaleFilter.java
+++ iMage/iMage.edge-detection-sobel/src/main/java/org/iMage/edge/detection/sobel/filter/GrayScaleFilter.java
@@ -1,22 +1,26 @@
 package org.iMage.edge.detection.sobel.filter;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 import org.iMage.edge.detection.base.ImageFilter;
 
-/**
- * Implements the GrayScaleFilter as requested on worksheet 2.
- */
 public class GrayScaleFilter implements ImageFilter {
 
-	/** Default constructor must be available! */
 	public GrayScaleFilter() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public BufferedImage applyFilter(BufferedImage image) {
-		// TODO Auto-generated method stub
-		return null;
+		BufferedImage result = image;
+		for (int i = 0; i < result.getHeight(); i++) {
+			{
+				for (int j = 0; j < result.getWidth(); j++) {
+					result.setRGB(j, i, image.getRGB(j, i) / 3);
+				}
+			}
+		}
+		return result;
 	}
 }
